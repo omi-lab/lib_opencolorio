80e628343eb2ec77afe35ab7d447e8b33ece2eae
Tom Paynter <tompaynter@tdpe.co.uk>
2022-01-28 13:45:15 +0000

    Fixed OptiX 7.3 build.

--- inc/OpenColorIO/utils/Half.h
+++ inc/OpenColorIO/utils/Half.h
@@ -5,9 +5,7 @@
 #ifndef INCLUDED_OCIO_IMATH_H
 #define INCLUDED_OCIO_IMATH_H
 
-#define OCIO_USE_IMATH_HALF 1
-
-#if OCIO_USE_IMATH_HALF
+#ifdef OCIO_USE_IMATH_HALF
 #   include <Imath/half.h>
 #else
 #   include <OpenEXR/half.h>
